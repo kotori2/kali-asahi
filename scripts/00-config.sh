3f71a83e16be4bbfffc10f1795013254f7f40b67
Tamir Suliman <allamir2002@yahoo.com>
2023-09-25 19:31:59 +0200

    Update 00-config.sh

--- scripts/00-config.sh
+++ scripts/00-config.sh
@@ -57,4 +57,46 @@ DISTS_DIR="${MNT_DIR}/dists"
 
 SED_PATTERN="s|CASPER_PATH|${CASPER_NAME}|g; s|DISTRO_NAME|${DISTRO_NAME}|g; s|DISTRO_VERSION|${DISTRO_VERSION}|g; s|DISTRO_EPOCH|${DISTRO_EPOCH}|g; s|DISTRO_DATE|${DISTRO_DATE}|g"
 
+_RED=$(tput setaf 1 || "")
+_GREEN=$(tput setaf 2 || "")
+_YELLOW=$(tput setaf 3 || "")
+_RESET=$(tput sgr0 || "")
+_BOLD=$(tput bold || "")
+_DIM=$(tput dim || "")
 
+function bold {
+	echo "${_BOLD}$@${_RESET}"
+}
+
+function info {
+	echo "[${_GREEN}${_BOLD}info${_RESET}] $@"
+}
+
+function error {
+	echo "[${_RED}${_BOLD}error${_RESET}] $@"
+}
+
+function warn {
+	echo "[${_YELLOW}${_BOLD}warning${_RESET}] $@"
+}
+
+function capture_and_log {
+	PREFIX="[${_GREEN}${_BOLD}$1${_RESET}] ${_DIM}"
+	SUFFIX="${_RESET}"
+	while read IN
+	do
+		echo "${PREFIX}${IN}${SUFFIX}"
+	done
+}
+
+# if [[ "${EUID:-$(id -u)}" != 0 ]]; then
+# 	error "This script must be run as root."
+# 	exit 1
+# fi
+
+# Source: https://stackoverflow.com/a/17841619
+function join_by { local IFS="$1"; shift; echo "$*"; }
+
+if [[ "$(uname -p)" -ne "aarch64" ]]; then
+	update-binfmts --enable
+fi
