4e8e2a6c87de057be5bfad8b50aa7873670aa44e
Tamir Suliman <allamir2002@yahoo.com>
2023-09-25 19:31:13 +0200

    Create 00-config.sh

---
+++ scripts/00-config.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+set -e
+
+export LC=C.UTF-8
+
+SOURCE_DATE_EPOCH="$(git --git-dir="$PWD/../.git" log -1 --format='%ct' 2> /dev/null || echo "42")"
+DISTRO_NAME=kali
+DISTRO_VERSION=$(grep VERSION /etc/os-release | awk -F '=' '{print $2}' | tr -d '"')
+DISTRO_VOLUME_LABEL="Kali Linux ${DISTRO_VERSION} arm64"
+DISTRO_EPOCH="${SOURCE_DATE_EPOCH}"
+DISTRO_DATE="$(date --date=@"${SOURCE_DATE_EPOCH}" +%Y%m%d)"
+
+DISTRO_PKGS=(kali-linux-core)  # Modify this according to your needs for base Kali packages
+LIVE_PKGS=(kali-linux)  # Modify this according to your needs for live Kali packages
+DISK_PKGS=(grub-efi kali-linux-full)  # Modify this according to your needs for disk Kali packages
+HOLD_PKGS=()
+RM_PKGS=()
+MAIN_POOL=()
+
+EFI_UUID=2ABF-9F91
+ROOT_UUID=87c6b0ce-3bb6-4dc2-9298-3a799bbb5994
+
+SCRIPTS_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+BUILD_DIR="$(realpath "${SCRIPTS_DIR}/../build")"
+CACHE_DIR="${BUILD_DIR}/cache"
+TMP_DIR="/tmp/kali-asahi.build/"
+SQUASHFS_FILE="${BUILD_DIR}/rootfs.squashfs"
+
+FS_DIR="$(realpath "${SCRIPTS_DIR}/../fs")"
+FS_COMMON_DIR="${FS_DIR}/common"
+FS_DISK_DIR="${FS_DIR}/disk"
+FS_LIVE_DIR="${FS_DIR}/live"
+FS_LIVE_EFI_DIR="${FS_DIR}/live-efi"
+FS_DISK_EFI_DIR="${FS_DIR}/disk-efi"
+FS_DEBS_DIR="${FS_DIR}/debs"
+FS_POOL_DIR="${FS_DIR}/pool"
+
+ROOTFS_BASE_DIR="${BUILD_DIR}/rootfs.base"
+ROOTFS_DISK_DIR="${BUILD_DIR}/rootfs.disk"
+ROOTFS_LIVE_DIR="${BUILD_DIR}/rootfs.live"
+
+CHROOT_MANIFEST="${BUILD_DIR}/chroot.manifest"
+LIVE_MANIFEST="${BUILD_DIR}/live.manifest"
+
+DISK_IMG_FILE="${BUILD_DIR}/kali.disk.img"
+LIVE_IMG_FILE="${BUILD_DIR}/kali.live.img"
+
+CASPER_NAME="casper"
+MNT_DIR="${BUILD_DIR}/mnt"
+DOT_DISK_INFO="${MNT_DIR}/.disk/info"
+CASPER_DIR="${MNT_DIR}/${CASPER_NAME}"
+FILESYSTEM_SIZE_TAG="${CASPER_DIR}/filesystem.size"
+ROOTFS_SQUASHED="${CASPER_DIR}/filesystem.squashfs"
+POOL_DIR="${MNT_DIR}/pool"
+MAIN_POOL_DIR="${POOL_DIR}/main"
+DISTS_DIR="${MNT_DIR}/dists"
+
+SED_PATTERN="s|CASPER_PATH|${CASPER_NAME}|g; s|DISTRO_NAME|${DISTRO_NAME}|g; s|DISTRO_VERSION|${DISTRO_VERSION}|g; s|DISTRO_EPOCH|${DISTRO_EPOCH}|g; s|DISTRO_DATE|${DISTRO_DATE}|g"
+
+
